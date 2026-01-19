e2520816363673baa656ae35fedbe8c5176cec0b
Berangere Br <brunberangere@gmail.com>
2026-01-19 20:08:59 +0100

    changement style écriture

--- src/components/portolio/Hero.jsx
+++ src/components/portolio/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
             <div className='accueil-container-avatar'>
                 <img src={avatar} alt='Bérangère Brun' width='250px'></img></div>
             <div className='accueil-container-text'>
-                <h1 className='accueil-text'>Bonjour, je suis< strong className='accueil-name'>Bérangère Brun</strong>développeuse frontend</h1>
+                <h1 className='accueil-text'>Bonjour, je suis< strong className='accueil-name'>Bérangère Brun</strong>développeuse web</h1>
                 <div className=' accueil-link'>
                     <a href='https://github.com/BerangereBr' target='_blank'><img src={github} alt='logo github' className="footer-logo" /></a>
                     <a href='https://www.linkedin.com/in/bérangère-brun-6208712a0' target='_blank'><img src={linkedin} alt='logo linkedin' className="footer-logo" /></a>
